bb29d470e7426a89ddf8a2a62927dd10a7ec97f5
Unknown <6433731@student.swin.edu.au>
2019-10-29 18:05:02 +1100

    made and fired off drowning animation

--- Assets/Scripts/SwimmerController.cs
+++ Assets/Scripts/SwimmerController.cs
@@ -5,13 +5,31 @@ using UnityEngine;
 public class SwimmerController : MonoBehaviour {
 
 	int StartPoint;
+	float swimAngle;
+	float terminus;
 	public float swimSpeed = 1;
+	public GameObject DrowningSwimmer;
+
+
+	SpriteRenderer ThisSR;
+
+	void Start()
+	{
+		ThisSR = GetComponent<SpriteRenderer> ();
+	}
 
 	void Awake () {
-		StartPoint = Random.Range (-7, 7);
+		StartPoint = Random.Range (-5, 5);
+		swimAngle = Random.Range (-1.0f, 1.0f);
+		terminus = Random.Range (0.0f, 1.0f);
+		if (swimAngle > 0) {
+			Vector3 tempScale = transform.localScale;
+			tempScale.x = -1;
+			transform.localScale = tempScale;
+		}
 		Vector3 temp = transform.position;
 		temp.x = StartPoint;
-		temp.y = 4;
+		temp.y = -2;
 		transform.position = temp;
 
 		Invoke ("MoveSwimmer", 1);
@@ -19,18 +37,26 @@ public class SwimmerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (transform.position.y <= -4) {
+		if (transform.position.y >= terminus) {
+			Instantiate (DrowningSwimmer, transform.position, Quaternion.identity);
 			Destroy (gameObject);
+		}
+
+		if (transform.position.y > 0.48f) {
+			ThisSR.sortingOrder = 1;
+		} else if (transform.position.y > -0.6f) {
+			ThisSR.sortingOrder = 3;
+		} else if (transform.position.y > -1.4f) {
+			ThisSR.sortingOrder = 5;
 		}
 	}
 
 	private void MoveSwimmer()
 	{
 		Vector3 temp = transform.position;
-		temp.y -= swimSpeed;
+		temp.y += swimSpeed;
+		temp.x += swimAngle;
 		transform.position = temp;
-
-
 
 		Invoke ("MoveSwimmer", 2);
 	}
